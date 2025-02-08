94140f5534ace945da85876e2efb718dd8cf6f78
imshy <imshi1@protonmail.com>
2025-02-07 20:58:34 -0500

    added client

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
-fn main() {
-    println!("Hello, world!");
+#![allow(unused)]
+
+use client::YahooFinanceClient;
+
+mod client;
+#[tokio::main]
+
+async fn main() {
+    let mut client = YahooFinanceClient::new().await.unwrap();
+    let output = client.fetch_quote_summary("AAPL").await.unwrap();
+    println!("{:?}", client);
+    println!("{:?}", output);
 }
